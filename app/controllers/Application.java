0066c5de00bec78ae93726a654e6246f31e7948c
josedynamo <joseramonrivera21@gmail.com>
2012-08-15 20:27:22 +0200

    added new UI to create Tasks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import models.Task;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -9,6 +11,13 @@ public class Application extends Controller {
   
   public static Result index() {
     return ok(index.render("Tasks App."));
+  }
+  
+  public static Result addTask(){
+	  Form<Task> form = form(Task.class).bindFromRequest();
+	  Task task = form.get();
+	  task.save();
+	  return redirect(routes.Application.index());
   }
   
 }
--- test/test/TaskTest.java
+++ test/test/TaskTest.java
@@ -21,6 +21,6 @@ public class TaskTest {
 				assertThat(task.Id).isNotNull();
 			}
 		});
-	}
+	}	
 
 }
