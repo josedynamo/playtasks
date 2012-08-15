6dd2aef1171216f93d0955d4b10ff7c0fd634710
josedynamo <joseramonrivera21@gmail.com>
2012-08-15 23:53:56 +0200

    having problem with the twitter bootstrap and heroku deploy

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,23 +1,41 @@
 package controllers;
 
-import models.Task;
-import play.*;
-import play.data.Form;
-import play.mvc.*;
+import static play.libs.Json.toJson;
 
-import views.html.*;
+import java.util.List;
+
+import models.Task;
+import play.data.Form;
+import play.db.ebean.Model;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Tasks App."));
-  }
-  
-  public static Result addTask(){
-	  Form<Task> form = form(Task.class).bindFromRequest();
-	  Task task = form.get();
-	  task.save();
-	  return redirect(routes.Application.index());
-  }
-  
+	final static Form<Task> taskForm = form(Task.class);
+
+	public static Result index() {
+		return ok(index.render("Tasks App.", taskForm));
+	}
+
+	public static Result addTask() {
+
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(index.render("Task Error", taskForm));
+		} else {
+			Task task = new Task(); 
+			task=filledForm.get();
+			task.save();
+			return redirect(routes.Application.index());
+	}
+		}
+	
+	
+
+	public static Result getTasks() {
+		List<Task> tasks = new Model.Finder(String.class, Task.class).all();
+		return ok(toJson(tasks));
+	}
+
 }
--- app/models/Task.java
+++ app/models/Task.java
@@ -3,15 +3,27 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
+
 
 
 @Entity
 public class Task extends Model {
 	
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
 	@Id
 	public String Id;
 	
+	
+	@Required
 	public String contents;
 
 }
